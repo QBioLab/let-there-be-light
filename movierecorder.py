50352b2be1b240cbbdfd71c9d1ddd7e4890104f5
khadas <moyuejian@outlook.com>
2021-06-23 17:36:47 +0800

    add movierecorder.py

---
+++ movierecorder.py
@@ -0,0 +1,24 @@
+import sys
+import cv2
+
+if __name__ == '__main__':
+    val = True
+    idx = int(sys.argv[1])
+    cap = cv2.VideoCapture(idx)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
+    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
+    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+    out = cv2.VideoWriter("record-"+str(idx)+".avi", fourcc, 30.0, (800, 600), True)
+
+    while val is True:
+        ret, frame = cap.read()
+        cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
+        if frame is None:
+            break
+        else:
+            out.write(frame)
+            print("*", end="")
+
+    cap.release()
+    out.release()
